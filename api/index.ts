798236ac953cab1b5157031e335bd25a4cb5c528
banananeo <prosabareesh@gmail.com>
2026-08-18 20:13:31 +0530

    refactor: export express app directly in API handler

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,8 @@
-import type { Request, Response } from "express";
 import { createApp } from "../server/app";
 
 const app = createApp();
 
-export default function handler(req: Request, res: Response) {
-  return app(req, res);
-}
+export default app;
+
 
 
